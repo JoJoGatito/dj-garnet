585b806fd5fcfc73d18d8c125fe638ec8e6dd6b0
JoJoGatito <jckurutteru@gmail.com>
2025-10-30 16:38:38 -0600

    Fix moon icon alignment in theme toggle

--- client/src/components/theme-toggle.tsx
+++ client/src/components/theme-toggle.tsx
@@ -21,7 +21,7 @@ export function ThemeToggle() {
       aria-label="Toggle theme"
     >
       <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="absolute top-2 left-2 h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </button>
   );
 }
